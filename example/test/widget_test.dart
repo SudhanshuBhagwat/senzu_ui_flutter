af3beece4e048b0341927e1927219f618a46d054
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2021-11-15 20:51:55 +0530

    Updated widget_test.dart

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:senzuiui/main.dart';
+import 'package:senzuiui_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
